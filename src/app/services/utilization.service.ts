3138135626d533c74cb9f1ba5b763cbe927e4a62
njc24 <njc24@njit.edu>
2017-05-16 15:48:26 -0400

    not sure if this fixes it

--- src/app/services/utilization.service.ts
+++ src/app/services/utilization.service.ts
@@ -16,19 +16,33 @@ export class UtilizationService {
                .catch(this.handleError);
   }
 
-  getNamespaces(): Promise<any> {
+  getNamespaces(): Promise<string[]> {
     return this.http.get('/api/namespaces')
                .toPromise()
-               .then(response => response.json())
+               .then(this.extractData)
                .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.namespaces || { };
   }
 
   /**
   * Handle any errors that may occur.
   */
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
+  private handleError (error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Promise.reject(errMsg);
   }
 
 }
