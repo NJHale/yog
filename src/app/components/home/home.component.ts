afa11eba2613e2949a79f1913cfbd983165fee12
NicholasCostanzo <costanzo.nicholas.j@gmail.com>
2017-05-30 16:36:40 -0400

    remove test items

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,6 +13,8 @@ import { UtilizationService } from '../../services/utilization.service';
 })
 export class HomeComponent implements OnInit {
 
+  private readonly SKIP_FREQ = 0;
+
   private historicalTotalMemUsed: number[] = new Array();
   private historicalTotalMemLimit: number[] = new Array();
   private historicalTotalCpuUsed: number[] = new Array();
@@ -100,14 +102,16 @@ export class HomeComponent implements OnInit {
         cpuLimitTotal += parseInt(utilization.cpuLimit);
       }
       skip++;
-      if(skip > 72) skip = 0;
+      if(skip > this.SKIP_FREQ) skip = 0;
     }
   }
 
+
+
   // memLineChart
   public memLineChartData:Array<any> = [
-    {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], label: 'Memory Used'},
-    {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], label: 'Memory Limit'}
+    {data: [], label: 'Memory Used'},
+    {data: [], label: 'Memory Limit'}
   ];
   memLineChartLabels:Array<any> = [];
 
@@ -119,12 +123,6 @@ export class HomeComponent implements OnInit {
   cpuLineChartLabels:Array<any> = [];
 
   // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions:any = {
     responsive: true,
     bezierCurve: false,
@@ -134,53 +132,28 @@ export class HomeComponent implements OnInit {
       }
     }
   };
-  public lineChartColors:Array<any> = [
-    { // grey
+  public lineChartType:string = 'line';
+  public lineChartLegend:boolean = true;
+  public lineChartColors:Array<any> = [{ // grey
       backgroundColor: 'rgba(148,159,177,0.2)',
       borderColor: 'rgba(148,159,177,1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
+    }, { // dark grey
       backgroundColor: 'rgba(77,83,96,0.2)',
       borderColor: 'rgba(77,83,96,1)',
       pointBackgroundColor: 'rgba(77,83,96,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
+    }, { // grey
       backgroundColor: 'rgba(148,159,177,0.2)',
       borderColor: 'rgba(148,159,177,1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-  ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
-
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
-
-  // events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
-
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
-
+    }];
 }
