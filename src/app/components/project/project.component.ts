cc5f045c5008e6952c01376d9567a1b87065b273
NicholasCostanzo <costanzo.nicholas.j@gmail.com>
2017-05-31 09:51:36 -0400

    start adding routing for project details page

---
+++ src/app/components/project/project.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { Utilization } from '../../models/utilization';
+
+import { UtilizationService } from '../../services/utilization.service';
+
+@Component({
+  selector: 'project-component',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
+})
+export class ProjectComponent {
+  
+}
